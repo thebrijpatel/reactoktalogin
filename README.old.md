1f662aa0a86b6c2a2be9d61cba39505dd8a1a789
Brijesh Patel <sadariyabrijesh@gmail.com>
2021-02-27 11:27:22 +0530

    Setting up the folder structure

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# reactoktalogin
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HW</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
